7fc042b8617d4db9aac8b3d703d756e75d6dc209
Joshua Sullivan <joshuasullivan@gmail.com>
2020-11-08 15:34:43 -0600

    Fixing access levels on objects in the library.

--- Sources/SimplexNoiseFilter/extension/CIColor.swift
+++ Sources/SimplexNoiseFilter/extension/CIColor.swift
@@ -1,6 +1,6 @@
 import CoreImage
 
-extension CIColor {
+public extension CIColor {
     /// Convert a CIColor into a CIVector[4] for use in Metal.
     var ciVector: CIVector {
         return CIVector(x: red, y: green, z: blue, w: alpha)
--- Sources/SimplexNoiseFilter/filter/SimplexNoiseGenerator.swift
+++ Sources/SimplexNoiseFilter/filter/SimplexNoiseGenerator.swift
@@ -5,45 +5,45 @@ import CoreImage
 /// The noise has a random structure, but transitions smoothly between values.
 /// This produces an effect similar to ripples on the surface of water.
 ///
-class SimplexNoiseGenerator: CIFilter {
+public final class SimplexNoiseGenerator: CIFilter {
 
     /// The color to use for the value of `0.0` in the noise output.
-    var lowColor: CIColor = .black
+    public var lowColor: CIColor = .black
 
     /// The color to use for the value of `1.0` in the noise output.
-    var highColor: CIColor = .white
+    public var highColor: CIColor = .white
 
     /// The distance to shift the noise space along the x-axis. Default value is `0.0`.
     ///
     /// The amount of change is affected by the `zoom` level.
-    var offsetX: Float = 0.0
+    public var offsetX: Float = 0.0
 
     /// The distance to shift the noise space along the y-axis. Default value is `0.0`.
     ///
     /// The amount of change is affected by the `zoom` level.
-    var offsetY: Float = 0.0
+    public var offsetY: Float = 0.0
 
     /// The distance to shift the noise space along the z-axis. Default value is `0.0`.
     ///
     /// Shifting along the z-axis has the effect of gradulally changing the shape of noise features without
     /// producing any apparent horizontal or vertical movement of the noise, overall. The amount of change
     /// is affected by the `zoom` level.
-    var offsetZ: Float = 0.0
+    public var offsetZ: Float = 0.0
 
     /// The amount to "zoom in" on the noise. Valid values are in the range [1...1000]. Default value is `100.0`.
     ///
     /// At a value of 1.0, the noise features are extremely small.
-    var zoom: Float = 100.0
+    public var zoom: Float = 100.0
 
     /// The amount of contrast increase or decrease in the final product. Default value is `1.0`.
     ///
     /// A contrast of 1.0 is linear contrast.
     /// A contrast in the range (1 > contrast >= 10) will increase the contrast of the output.
     /// A contrast in the range (0.1 >= contrast > 1) will decrease the contrast of the output.
-    var contrast: Float = 1.0
+    public var contrast: Float = 1.0
 
     /// Set up the kernel by loading the metallib from the bundle.
-    static var kernel: CIColorKernel? = {
+    private static var kernel: CIColorKernel? = {
         guard let url = Bundle.main.url(forResource: "SimplexNoise", withExtension: "ci.metallib") else { return nil }
 
         do {
@@ -55,7 +55,7 @@ class SimplexNoiseGenerator: CIFilter {
         return nil
     }()
 
-    override var outputImage: CIImage? {
+    public override var outputImage: CIImage? {
         guard let kernel = SimplexNoiseGenerator.kernel else {
             print("Failed to create kernel.")
             return nil
